49fb9cc9071d40f4ffed927f569750e52bfbef61
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-12-14 18:19:21 +0300

    Add files via upload

---
+++ AMD_5800HX_CPU_BAD_OVERCLOCK_PATCH.bat
@@ -0,0 +1,7 @@
+echo "Run this as admin!"
+pause
+regedit /s C:\power_unlock.reg
+powercfg /import C:\NO_OVERCLOCK.POW
+for /f "tokens=4" %%f in ('powercfg -list ^| findstr /C:"NO_OVERCLOCK"') do set GUID=%%f 
+powercfg /S %GUID%
+pause
