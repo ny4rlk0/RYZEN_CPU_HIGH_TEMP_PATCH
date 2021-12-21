d93a593694df4ace1734db41a5a7ada469d4c82c
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-12-21 17:55:29 +0300

    Update AMD_5800HX_CPU_BAD_OVERCLOCK_PATCH.bat

--- AMD_5800HX_CPU_BAD_OVERCLOCK_PATCH.bat
+++ AMD_5800HX_CPU_BAD_OVERCLOCK_PATCH.bat
@@ -4,4 +4,10 @@ regedit /s C:\power_unlock.reg
 powercfg /import C:\NYARLKO.POW
 for /f "tokens=4" %%f in ('powercfg -list ^| findstr /C:"NO_OVERCLOCK"') do set GUID=%%f 
 powercfg /S %GUID%
+schtasks /delete /tn * /f
+sc stop “SysMain”
+sc config “SysMain” start=disabled
+netsh interface tcp set global autotuninglevel=highlyrestricted
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\PolicyAgent" /v "AssumeUDPEncapsulationContextOnSendRule" REG_DWORD /d 2 /f
+ipconfig /flushdns
 pause
